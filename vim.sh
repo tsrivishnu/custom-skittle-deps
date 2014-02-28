0dcf1691c996c6f0d08e8abbdebcee0d70f1814a
Michael Sauter <mail@michaelsauter.net>
2014-02-28 14:25:30 +1100

    Add .vimrc and rename dep to vim

--- vim.sh
+++ vim.sh
@@ -1,4 +1,4 @@
-vim_solarized() {
+vim() {
 
   is_met() {
     exit 0
@@ -8,5 +8,6 @@ vim_solarized() {
     exit 0
   }
 
+  require symlink_exists $deps_path/lib/vim/vimrc.symlink $HOME/.vimrc
   require file_downloaded $HOME/.vim/colors/solarized.vim https://raw.github.com/altercation/vim-colors-solarized/master/colors/solarized.vim
 }
