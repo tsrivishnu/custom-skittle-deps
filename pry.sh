6bbd35a148e01025f5e4072322f006909d22eb04
Michael Sauter <mail@michaelsauter.net>
2014-03-07 11:28:44 +1100

    Add Rails Console helpers to Pry

--- pry.sh
+++ pry.sh
@@ -2,4 +2,5 @@ pry() {
   deps_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
   require symlink_exists $deps_path/lib/pry/irbrc.symlink $HOME/.irbrc
+  require symlink_exists $deps_path/lib/pry/pryrc.symlink $HOME/.pryrc
 }
