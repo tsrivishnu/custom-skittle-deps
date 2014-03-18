939adfdf8212555c955e01ffc13e6748f16f4428
Michael Sauter <mail@michaelsauter.net>
2014-03-18 12:25:15 +1100

    Use alternative apt name for ack

--- ack.sh
+++ ack.sh
@@ -1,6 +1,6 @@
 ack() {
   deps_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-  require package_installed ack
+  require package_installed ack ack-grep
   require symlink_exists $deps_path/lib/ack/ackrc.symlink $HOME/.ackrc
 }
