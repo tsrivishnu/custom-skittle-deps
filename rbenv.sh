f5ec4c90ca00c4ccf87e36d95eb3b75415a7bbe1
Michael Sauter <mail@michaelsauter.net>
2014-03-21 11:52:08 +1100

    Install rbenv and ruby-build with Homebrew

--- rbenv.sh
+++ rbenv.sh
@@ -1,6 +1,7 @@
 rbenv() {
   deps_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-  require repo_cloned https://github.com/sstephenson/rbenv.git $HOME/.rbenv
+  require package_installed rbenv
+  require package_installed ruby-build
   require symlink_exists $deps_path/lib/rbenv/zsh-loaded $deps_path/zsh-loaded/rbenv
 }
