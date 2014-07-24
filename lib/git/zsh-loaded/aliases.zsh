4ee1c739001bb6067d81a4b7e0f119466408d0e2
Michael Sauter <mail@michaelsauter.net>
2014-07-24 01:47:35 +0000

    Remove gitpurge

--- lib/git/zsh-loaded/aliases.zsh
+++
@@ -1,3 +0,0 @@
-# Clean up merged remote branches
-# https://blog.splice.com/cleaning-git-branches/
-gitpurge = "git checkout master && git remote update --prune | git branch --remote --merged | grep --invert-match master | sed -e 's/origin\//:/' | xargs git push origin"
