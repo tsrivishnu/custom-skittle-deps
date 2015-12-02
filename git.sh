21edc07cb5ad8b14b3f9386dc3204d8d5980c9e3
Michael Sauter <mail@michaelsauter.net>
2015-12-02 10:31:20 +0100

    Install git-when-merged

--- git.sh
+++ git.sh
@@ -2,4 +2,14 @@ git() {
   require package_installed git
   require symlink_exists $p/lib/git/gitconfig.symlink $HOME/.gitconfig
   require symlink_exists $p/lib/git/gitignore.symlink $HOME/.gitignore
+
+  is_met() {
+    which git-when-merged
+  }
+
+  meet() {
+    git clone git@github.com:mhagger/git-when-merged.git /tmp/git-when-merged
+    sudo cp /tmp/git-when-merged/bin/git-when-merged /usr/local/bin/
+    rm -rf /tmp/git-when-merged
+  }
 }
