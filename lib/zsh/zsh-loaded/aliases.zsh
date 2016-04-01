05fe63294bcc95e538ac1fd8eed3c6263afe9539
Michael Sauter <michael.sauter@experteer.com>
2016-04-01 13:34:50 +0200

    Remove deprecated GREP_OPTIONS

--- lib/zsh/zsh-loaded/aliases.zsh
+++ lib/zsh/zsh-loaded/aliases.zsh
@@ -1,4 +1,5 @@
 # Aliases
 alias l='ls -lAh'
 alias rm._='find . -name "._?*" -exec rm {} \;'
+alias grep='grep --color=auto'
 alias weather='curl http://wttr\.in/Munich'
--- lib/zsh/zsh-loaded/colors.zsh
+++ lib/zsh/zsh-loaded/colors.zsh
@@ -7,6 +7,3 @@ export LSCOLORS='Gxfxcxdxbxegedabagacad'
 # Enable ls colors
 ls --color -d . &>/dev/null 2>&1 && alias ls='ls --color=tty' || alias ls='ls -G'
 
-# Enable color in grep
-export GREP_OPTIONS='--color=auto'
-export GREP_COLOR='3;33'
