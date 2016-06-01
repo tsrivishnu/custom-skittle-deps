eb743de15af86ede00d534cfb3acbab15bae3f55
Sri Vishnnu Totakura <srivishnu@totakura.in>
2016-06-01 16:39:24 +0200

    remove alias from gvim install script

--- gvim.sh
+++ gvim.sh
@@ -2,22 +2,7 @@ gvim() {
   require package_installed gvim vim-gnome
   require symlink_exists $p/lib/gvim/vimrc.symlink $HOME/.vimrc
   require repo_cloned https://github.com/gmarik/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
-  require vim_alias
   require backups_folder
-
-  vim_alias(){
-    alias_text="vim='gvim'"
-
-    is_met(){
-      #TODO: Probably there is a much better way to find if the alias exists.
-      less $HOME/.bashrc | grep $alias_text
-    }
-
-    meet(){
-      echo "alias $alias_text" >> $HOME/.bashrc
-      source $HOME/.bashrc
-    }
-  }
 
   backups_folder(){
     vim_backup_dir=$HOME/.vim/backup
