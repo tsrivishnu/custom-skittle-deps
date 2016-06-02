33c840bc68ef0ed106532328487daf50ef41205c
Sri Vishnnu Totakura <srivishnu@totakura.in>
2016-06-02 12:48:57 +0200

    fix for commands with wildcard operators in zsh

--- lib/zsh/zsh-loaded/setopt.zsh
+++ lib/zsh/zsh-loaded/setopt.zsh
@@ -18,12 +18,13 @@ setopt hist_reduce_blanks # Remove extra blanks from each command line being add
 setopt hist_verify # don't execute, just expand history
 setopt share_history # imports new commands and appends typed commands to history
 
-# ===== Completion 
-setopt always_to_end # When completing from the middle of a word, move the cursor to the end of the word    
+# ===== Completion
+setopt always_to_end # When completing from the middle of a word, move the cursor to the end of the word
 setopt auto_menu # show completion menu on successive tab press. needs unsetop menu_complete to work
 setopt auto_name_dirs # any parameter that is set to the absolute name of a directory immediately becomes a name for that directory
 setopt complete_in_word # Allow completion from within a word/phrase
 unsetopt menu_complete # do not autoselect the first completion entry
+unsetopt nomatch  # no error if glob fails to expand (scp fix) (or fix for any other commands that use wildcard in options)
 
 # ===== Correction
 setopt correct # spelling correction for commands
