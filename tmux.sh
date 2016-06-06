dc9e56bd3a1177bd72493613a82230f32148dd34
Sri Vishnnu Totakura <srivishnu@totakura.in>
2016-06-06 14:21:23 +0200

    tmux OS-specific configuration

--- tmux.sh
+++ tmux.sh
@@ -1,5 +1,7 @@
 tmux() {
   require package_installed tmux
   require symlink_exists $p/lib/tmux/tmux.conf.symlink $HOME/.tmux.conf
+  require symlink_exists $p/lib/tmux/tmux-macosx.conf.symlink $HOME/.tmux-macosx.conf
+  require symlink_exists $p/lib/tmux/tmux-linux.conf.symlink $HOME/.tmux-linux.conf
   require symlink_exists $p/lib/tmux/zsh-loaded $p/zsh-loaded/tmux
 }
