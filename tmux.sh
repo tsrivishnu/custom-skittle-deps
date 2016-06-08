6720e2cd19720d6c67d86251a6ba03d2150b5e5f
Sri Vishnnu Totakura <srivishnu@totakura.in>
2016-06-09 00:47:14 +0200

    reattach-to-user-namespace on mac for tmux
    refer:
    https://www.elmund.io/osx/2015/07/10/open-command-in-osx-tmux/

--- tmux.sh
+++ tmux.sh
@@ -4,4 +4,11 @@ tmux() {
   require symlink_exists $p/lib/tmux/tmux-macosx.conf.symlink $HOME/.tmux-macosx.conf
   require symlink_exists $p/lib/tmux/tmux-linux.conf.symlink $HOME/.tmux-linux.conf
   require symlink_exists $p/lib/tmux/zsh-loaded $p/zsh-loaded/tmux
+
+  if [[ "$(uname)" = "Darwin" ]]; then
+    # https://www.elmund.io/osx/2015/07/10/open-command-in-osx-tmux/
+    require package_installed reattach-to-user-namespace
+    brew upgrade reattach-to-user-namespace
+  fi
+
 }
