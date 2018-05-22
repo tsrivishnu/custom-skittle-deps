0bc1c3c0bb252b56826d93b7a08cd5bf102a6b78
Sri Vishnnu Totakura <srivishnu@totakura.in>
2018-05-22 11:19:22 +0200

    Add rofi file-find script

--- rofi.sh
+++ rofi.sh
@@ -1,3 +1,7 @@
 rofi() {
   require package_installed rofi
+  # Below is the command you probably like to use.
+  # Requires file-find.sh
+  require symlink_exists $p/lib/rofi/file-find.sh.symlink $HOME/file-find.sh
+  # rofi -tokenize -show combi -combi-modi "window,drun,ssh,run,find:/home/stotakura/file-find.sh" -modi combi
 }
