ce042587507f5b6eaea4d81aa8a6f290354a7faf
Sri Vishnnu Totakura <srivishnu@totakura.in>
2018-08-17 14:55:42 +0200

    Update +pip+ installation

--- generic/pip.sh
+++ generic/pip.sh
@@ -6,7 +6,13 @@ pip() {
   meet() {
     curl -O https://bootstrap.pypa.io/get-pip.py;
     sudo python get-pip.py
-    rm get-pip.py
+    sudo apt-get install -y zlib1g-dev # (needed to compile Python)
+    sudo apt-get install -y libreadline-gplv2-dev \
+           		libncursesw5-dev \
+			libssl-dev \
+			libsqlite3-dev \
+			tk-dev libgdbm-dev \
+			libc6-dev libbz2-dev 
   }
 
 }
