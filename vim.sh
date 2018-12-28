c813e454b1928d0effc5dfb68960bb14a23a1914
Sri Vishnnu Totakura <srivishnu@totakura.in>
2018-12-28 12:33:23 +0100

    Update vim installation

--- vim.sh
+++ vim.sh
@@ -1,5 +1,6 @@
 vim() {
-  require package_installed vim
+  # use +vim-gnome+ so that vim installs with ++clipboard+
+  require package_installed vim-gnome vim
   require symlink_exists $p/lib/vim/vimrc.symlink $HOME/.vimrc
   require repo_cloned https://github.com/gmarik/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
   require dir_exists $HOME/.vim/backup
