fd16dbbe6cc9f91f74925e634f6ea7c575f5a822
Sri Vishnnu Totakura <srivishnu@totakura.in>
2019-03-12 11:20:55 +0100

    Remove unwanted code

--- fix-keyboard-tilde.sh
+++ fix-keyboard-tilde.sh
@@ -1,14 +1,4 @@
 fix-keyboard-tilde() {
-  # is_met() {
-  #   launchctl list | grep com.keyboard.swap-tilde
-  # }
-
-  # meet() {
-  #   cp $p/lib/mac-tilde-swap/com.keyboard.swap-tilde.plist $HOME/Library/LaunchAgents/
-  #   # sudo chown root:wheel $HOME/Library/LaunchAgents/com.keyboard.swap-tilde.plist
-  #   # sudo launchctl load -w $HOME/Library/LaunchAgents/com.keyboard.swap-tilde.plist
-  # }
-
   require symlink_exists $p/lib/mac-tilde-swap/com.keyboard.swap-tilde.plist $HOME/Library/LaunchAgents/com.keyboard.swap-tilde.plist
   require symlink_exists $p/lib/mac-tilde-swap/keyboard-fix-tilde.sh /usr/local/bin/swap-tilde-on-keyboard.sh
 }
