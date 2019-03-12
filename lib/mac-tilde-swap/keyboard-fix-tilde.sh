5efbaec5e0eef28179a23b8cdd781007cb468dd6
Sri Vishnnu Totakura <srivishnu@totakura.in>
2019-03-12 11:21:33 +0100

    Do the keyboard re-mapping for tilde only on specific keyboards

--- lib/mac-tilde-swap/keyboard-fix-tilde.sh
+++ lib/mac-tilde-swap/keyboard-fix-tilde.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # Following the instrutions from https://jonnyzzz.com/blog/2017/12/04/macos-keys/
-hidutil property --set '{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x700000064,"HIDKeyboardModifierMappingDst":0x700000035},{"HIDKeyboardModifierMappingSrc":0x700000035,"HIDKeyboardModifierMappingDst":0x700000064}]}'
+# Do this only for the built-in keyboard using the product ID. This ID used
+# is for the keyboard on MacBook (Retina, 12-inch, Early 2015)
+hidutil property --matching '{"ProductID":0x0272}'  --set '{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x700000064,"HIDKeyboardModifierMappingDst":0x700000035},{"HIDKeyboardModifierMappingSrc":0x700000035,"HIDKeyboardModifierMappingDst":0x700000064}]}'
 
 echo $(date) > $HOME/Desktop/date
