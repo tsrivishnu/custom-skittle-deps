73553aeee751655f988da18011032cccd836896f
Sri Vishnu Totakura <srivishnu@totakura.in>
2019-11-14 11:04:43 +0100

    HiDPI for Dell U2719DC

--- mac_force_hidpi.sh
+++ mac_force_hidpi.sh
@@ -1,8 +1,7 @@
 mac_force_hidpi() {
   # Forces HiDPI for external monitors.
 
-  # For DELL U2515H.
-  # Remember, your Dell's +DisplayProductID+ might be different
+  # Remember, your monitor's +DisplayProductID+ might be different
   # than the one that is used here. Follow the instructions in the URLs below
   # to find the right one.
   # Following the instructions from https://comsystoreply.de/blog-post/force-hidpi-resolutions-for-dell-u2515h-monitor
@@ -10,5 +9,8 @@ mac_force_hidpi() {
   # https://comsysto.github.io/Display-Override-PropertyList-File-Parser-and-Generator-with-HiDPI-Support-For-Scaled-Resolutions/
   echo "Attempting to create symlinkk into a system directory. If this fails, try running skittle as sudo"
   sudo defaults write /Library/Preferences/com.apple.windowserver.plist DisplayResolutionEnabled -bool true
-  require symlink_exists $p/lib/mac-hidpi-resolution-fix/dell-u2525H /System/Library/Displays/Contents/Resources/Overrides/DisplayVendorID-10ac/DisplayYearManufacture-2015-DisplayWeekManufacture-44
+  require symlink_exists $p/lib/mac-hidpi-resolution-fix/dell-u2719dc /System/Library/Displays/Contents/Resources/Overrides/DisplayVendorID-10ac/DisplayProductID-419C
+  # If it doesn't seem to work. Try this project: https://github.com/syscl/Enable-HiDPI-OSX
+
+  echo "Use RDM app to choose a scaled reslution"
 }
