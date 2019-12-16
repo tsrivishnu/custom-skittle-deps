e075e048ff8a13d5b8c2ccd38d2a4346161445e6
Sri Vishnu Totakura <srivishnu@totakura.in>
2019-12-16 14:46:50 +0000

    Remove gnome specific deps from bootstrap
    Mostly using only ubuntu server than desktop lately.

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,14 +6,9 @@ bootstrap() {
   require tmux
   require package_installed wget
   require package_installed ag silversearcher-ag the_silver_searcher
-  require redshift
-  requrie fzf
-  require rofi
+  require fzf
   require vim
   require package_installed htop
   require package_installed nethogs
   require package_installed tree
-
-  require customize-evolution
-  require gnome-tweaks
 }
