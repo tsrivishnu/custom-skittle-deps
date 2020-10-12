16b157dcd33b01dc26ac4b3557f6332cfb4cdfc4
Sri Vishnu Totakura <srivishnu@totakura.in>
2020-10-12 16:08:33 +0200

    Add rbenv and yarn to mac bootstrap

--- mac_bootstrap.sh
+++ mac_bootstrap.sh
@@ -2,4 +2,11 @@ mac_bootstrap() {
   require bootstrap
   require iterm2
   require rectangleapp
+
+  # rbenv
+  require package_installed rbenv
+  require symlink_exists $p/lib/rbenv/zsh-loaded $p/zsh-loaded/rbenv
+
+  # yarn
+  require package_installed yarn
 }
--- rbenv.sh
+++ rbenv.sh
@@ -1,5 +1,4 @@
 rbenv() {
-  require repo_cloned https://github.com/sstephenson/rbenv.git ~/.rbenv
-  require repo_cloned https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
-  require symlink_exists $p/lib/rbenv/zsh-loaded $p/zsh-loaded/rbenv
+  # require repo_cloned https://github.com/sstephenson/rbenv.git ~/.rbenv
+  # require repo_cloned https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
 }
