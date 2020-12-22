a629d768416305d6ebab1a992103842b25e12bfd
Sri Vishnu Totakura <srivishnu@totakura.in>
2020-12-22 13:34:30 +0100

    Fix Mac HiDPI on BigSur

--- mac_force_hidpi.sh
+++ mac_force_hidpi.sh
@@ -1,12 +1,5 @@
 # Prerequisites
 # - SIP OFF
-# - Root filesystem mounted for write
-#   - Otherwise, you get +Read-only filesystem+ error when copying files to library
-#   ```
-#   sudo mount -uw /
-#   sudo killall Finder
-#   ```
-#   See: https://www.reddit.com/r/MacOS/comments/caiue5/macos_catalina_readonly_file_system_with_sip/
 
 mac_force_hidpi() {
   # Forces HiDPI for external monitors.
@@ -18,8 +11,13 @@ mac_force_hidpi() {
   # and
   # https://comsysto.github.io/Display-Override-PropertyList-File-Parser-and-Generator-with-HiDPI-Support-For-Scaled-Resolutions/
   echo "Attempting to create symlinkk into a system directory. If this fails, try running skittle as sudo"
+
+  # Using +/Library/+ directory instead of the +/System/Library/+ because BigSur
+  # doesn't seem to let you write to the +/System/+ even with SIP off.
+  # Got this info from: https://gist.github.com/ejdyksen/8302862
+  require dir_exists /Library/Displays/Contents/Resources/Overrides/DisplayVendorID-10ac
   sudo defaults write /Library/Preferences/com.apple.windowserver.plist DisplayResolutionEnabled -bool true
-  require symlink_exists $p/lib/mac-hidpi-resolution-fix/dell-u2719dc /System/Library/Displays/Contents/Resources/Overrides/DisplayVendorID-10ac/DisplayProductID-419C
+  require symlink_exists $p/lib/mac-hidpi-resolution-fix/dell-u2719dc /Library/Displays/Contents/Resources/Overrides/DisplayVendorID-10ac/DisplayProductID-419C
   # If it doesn't seem to work. Try this project: https://github.com/syscl/Enable-HiDPI-OSX
 
   echo "Use RDM app to choose a scaled reslution"
