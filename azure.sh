dfa8445f48d2d602287072d0ad6f1f4b7ad50c90
Sri Vishnu Totakura <srivishnu@totakura.in>
2021-06-30 09:54:37 +0200

    Update azure installation script

--- azure.sh
+++ azure.sh
@@ -8,8 +8,8 @@ azure() {
     # see: https://docs.microsoft.com/en-us/cli/azure/install-azure-cli-linux?pivots=apt
     sudo apt update
     sudo apt-get -y install ca-certificates curl apt-transport-https lsb-release gnupg
-    curl -sL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/microsoft.gpg > /dev/null
-echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/azure-cli.list
+    curl -fsSL https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+    sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $(lsb_release -cs) main"
     sudo apt update
     sudo apt install -y azure-cli
   }
