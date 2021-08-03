447093ed9cf200eb4f61074a1ba252e502e7c591
Sri Vishnu Totakura <srivishnu@totakura.in>
2021-08-03 16:44:16 +0200

    Update azure-cli for Mac

--- azure.sh
+++ azure.sh
@@ -5,12 +5,16 @@ azure() {
   }
 
   meet() {
-    # see: https://docs.microsoft.com/en-us/cli/azure/install-azure-cli-linux?pivots=apt
-    sudo apt update
-    sudo apt-get -y install ca-certificates curl apt-transport-https lsb-release gnupg
-    curl -fsSL https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
-    sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $(lsb_release -cs) main"
-    sudo apt update
-    sudo apt install -y azure-cli
+    if [[ "$(uname)" = "Darwin" ]]; then
+      brew install azure-cli
+    else
+      # see: https://docs.microsoft.com/en-us/cli/azure/install-azure-cli-linux?pivots=apt
+      sudo apt update
+      sudo apt-get -y install ca-certificates curl apt-transport-https lsb-release gnupg
+      curl -fsSL https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+      sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $(lsb_release -cs) main"
+      sudo apt update
+      sudo apt install -y azure-cli
+    fi
   }
 }
