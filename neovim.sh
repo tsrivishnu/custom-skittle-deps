48b757e0d1f3c7b5e2316f5088cd4c487d385c5e
Sri Vishnu Totakura <srivishnu@totakura.in>
2022-02-09 20:32:54 +0100

    Add vim-plug and markdown preview plugin

--- neovim.sh
+++ neovim.sh
@@ -1,8 +1,20 @@
+vim_plug() {
+  is_met() {
+    ls $HOME/.vim/autoload/plug.vim
+  }
+
+  meet() {
+    curl -fLo $HOME/.vim/autoload/plug.vim --create-dirs \
+      https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+  }
+}
+
 neovim() {
   # use +vim-gnome+ so that vim installs with ++clipboard+
   require package_installed nvim neovim neovim
   require symlink_exists $p/lib/vim/vimrc.symlink $HOME/.vimrc
   require repo_cloned https://github.com/gmarik/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
+  require vim_plug
   require dir_exists $HOME/.vim/backup
   require dir_exists $HOME/.vim/undo
 
