746aee34532bf049c8598899ea5603e767e4dfd1
Sri Vishnu Totakura <srivishnu@totakura.in>
2022-02-10 09:08:59 +0100

    Add Dell u2720q color profile

---
+++ u2720q_color_profile.sh
@@ -0,0 +1,5 @@
+u2720q_color_profile() {
+  # Profile is downloaded from Rtings.com.
+  # See: https://www.rtings.com/monitor/reviews/dell/ultrasharp-u2720q
+  require symlink_exists $p/lib/u2720q-color-profile/up2720q-rtings-icc-profile.icm $HOME/Library/ColorSync/Profiles/up2720q-rtings-icc-profile.icm
+}
